194450275bdde3584f5229111a26b0977bd48aec
Will C <wcrain@nevada.unr.edu>
2019-12-23 06:39:56 +0000

    Fixed some math affecting angle calculations; Added dynamic minimum angle padding according to smallest data proporition

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let pieChart = function(x0, x1, y0, y1, name, p = 2, iRad = 0.7, pAngle = Math.PI/200) {
+let pieChart = function(x0, x1, y0, y1, name, p = 5, iRad = 0.75, pAngle = Math.PI/200) {
 	this.data = {}
 
 	this.x0 = 		x0
@@ -7,6 +7,8 @@ let pieChart = function(x0, x1, y0, y1, name, p = 2, iRad = 0.7, pAngle = Math.P
 	this.y1 = 		y1
 	this.p = 		p
 	this.pAngle = 	pAngle
+
+	this.minData = 	1e100
 
 	this.lastAngle = [0]
 
@@ -17,20 +19,26 @@ let pieChart = function(x0, x1, y0, y1, name, p = 2, iRad = 0.7, pAngle = Math.P
 	this.sum = 		0
 
 	this.construct = function() {
-		d3.select('body').select('svg').append('line').attr('id', 'TBound' + name)
+		// d3.select('body').select('svg').append('line').attr('id', 'VLine' + name)
+		// 	.attr('x1', (x0+x1)/2).attr('x2', (x0+x1)/2)
+		// 	.attr('y1', y0+p).attr('y2', y1-p)
+		// 	.attr('stroke-width', 2).style('stroke', 'black')
+
+
+		d3.select('body').select('svg').append('line').attr('id', 'BBound' + name)
 			.attr('x1', x0+p).attr('x2', x1-p)
 			.attr('y1', y1-p).attr('y2', y1-p)
 			.attr('stroke-width', 2).style('stroke', 'black')
 		d3.select('body').select('svg').append('line').attr('id', 'LBound' + name)
 			.attr('x1', x0+p).attr('x2', x0+p)
 			.attr('y1', y0+p).attr('y2', y1-p)
-			.attr('stroke-width', p).style('stroke', 'black')
+			.attr('stroke-width', 2).style('stroke', 'black')
 		
-		d3.select('body').select('svg').append('line').attr('id', 'TBound' + name)
+		d3.select('body').select('svg').append('line').attr('id', 'RBound' + name)
 			.attr('x1', x1-p).attr('x2', x1-p)
 			.attr('y1', y0+p).attr('y2', y1-p)
 			.attr('stroke-width', 1).style('stroke', 'grey')
-		d3.select('body').select('svg').append('line').attr('id', 'RBound' + name)
+		d3.select('body').select('svg').append('line').attr('id', 'TBound' + name)
 			.attr('x1', x0+p).attr('x2', x1-p)
 			.attr('y1', y0+p).attr('y2', y0+p)
 			.attr('stroke-width', 1).style('stroke', 'grey')
@@ -42,8 +50,9 @@ let pieChart = function(x0, x1, y0, y1, name, p = 2, iRad = 0.7, pAngle = Math.P
 	}
 
 	this.setup({x0: x0, x1: x1, y0: y0, y1: y1})
-	// this.construct({x0: x0, x1: x1, y0: y0, y1: y1})
+	this.construct({x0: x0, x1: x1, y0: y0, y1: y1})
 }
+
 pieChart.prototype.addData = function(datum) {
 	/* Datum is of the type
 	 * {label: [String], v: [Number], opts: {}, sub: {}}
@@ -53,13 +62,18 @@ pieChart.prototype.addData = function(datum) {
 	 * {[Datum]}
 	 */ 
 
-	this.data[datum.label] = datum
-	this.sum +=	datum.v
+	this.data[datum.label] = 	datum
+	this.sum +=					datum.v
+
+	this.minData = 				Math.min(datum.v, this.minData)
+	this.pAngle = 				(this.minData/this.sum)*(3/4)
+
 }
 pieChart.prototype.removeData = function(label) {
 	this.sum -= this.data[label].v
 	delete this.data[label]
 }
+
 pieChart.prototype.render = function() {
 	d3.select('body').append('svg').attr('width', this.x0+this.x1).attr('height', this.y0+this.y1)
 
@@ -67,40 +81,48 @@ pieChart.prototype.render = function() {
 		let datum = this.data[i]
 		this.renderArc(datum)
 	}
-
-	//
 }
-pieChart.prototype.renderArc = function(datum, depth = 0, numChildren = Object.keys(this.data).length, toFill = Math.PI*2) {
-	let angle = (Math.PI*2) * (datum.v/this.sum)
-	// let fillAngle = toFill - this.pAngle*numChildren - (depth > 0 ? this.pAngle : 0)
-	// let angle = (this.lastAngle[depth]) + fillAngle * (datum.v/this.sum)
+pieChart.prototype.renderArc = function(datum, depth = 0, numChildren = Object.keys(this.data).length, parentAngle = Math.PI*2, parentSum = this.sum) {
+
+	let toFill
+	if (depth == 0) {
+		if (numChildren == 1) {
+			toFill = parentAngle
+		}
+		else {
+			toFill = parentAngle
+		}
+	}
+	else if (depth > 0) {
+		toFill = parentAngle - ((numChildren+1) * this.pAngle)
+	}
+
+	let thisAngle = toFill*(datum.v/parentSum)
 
 	let startAngle = 	this.lastAngle[depth]
-		//  + (this.lastAngle[depth] == 0 ? 0 : this.pAngle/2)
-		//  + depth*this.pAngle/2
-	let endAngle = 		startAngle + angle 
-		// - this.pAngle/2
-		// - depth*this.pAngle/2
+	let endAngle = 		startAngle + thisAngle
 	
 	let arc = d3.arc()
 		.innerRadius(this.r0)
-		.outerRadius(this.r - depth*10)
+		.outerRadius(this.r - depth*10 - this.p)
 		.padAngle(this.pAngle)
 	
 	d3.select('body').select('svg').append('path')
-		.attr('d', arc({startAngle: startAngle, endAngle: endAngle}))
+		.attr('d', arc({startAngle: startAngle + this.pAngle/2, endAngle: endAngle - this.pAngle/2}))
 		// .attr('stroke-width', depth == 0 ? 2 : 0)
 		.attr('stroke', datum['opts']['border'])
 		.attr('fill', datum['opts']['fill'])
 		.attr('transform', `translate(${this.x0 + this.r + this.p}, ${this.y0 + this.r + this.p})`)
+		.attr('name', this.label)
 
+	console.log(`${'  '.repeat(depth)}${datum.label}\t${(datum.v/this.sum).toFixed(5)}\t(${datum.v}\t${(thisAngle/Math.PI).toFixed(3)}\t${(startAngle/Math.PI/2).toFixed(2)}\t${(endAngle/Math.PI/2).toFixed(2)})`)
+	console.log(`${'  '.repeat(depth)}${datum.label}\t${(thisAngle/Math.PI/2).toFixed(2)}\t${(toFill/Math.PI/2).toFixed(2)}`)
 	if (Object.keys(datum.sub).length > 0) {
 		let newDepth = depth+1
-		this.lastAngle[newDepth] = this.lastAngle[depth]
+		this.lastAngle[newDepth] = this.lastAngle[depth] + this.pAngle*2
 
 		for (let i in datum.sub) {
-			console.log(`${i}\t${datum.sub[i].v/this.sum}\t(${datum.sub[i].v}\t${this.sum})`)
-			this.renderArc(datum.sub[i], newDepth)//, Object.keys(datum.sub).length, angle)
+			this.renderArc(datum.sub[i], newDepth, Object.keys(datum.sub).length, thisAngle, datum.v)
 		}
 	}
 	
@@ -125,19 +147,24 @@ Data.prototype.addSubs = function(data) {
 	}
 }
 
+//		//		//		//		//		//		//
 
 let DEM = 		d3.hsl(240, 0.50, 0.50)
 let REP = 		d3.hsl(000, 0.50, 0.50)
 let IND =		d3.hsl(000, 0.00, 0.50)
 
+let TS0 = 		d3.hsl(000, 0.00, 0.70)
+let TS1 = 		d3.hsl(000, 0.00, 0.50)
+let TS2 = 		d3.hsl(000, 0.00, 0.30)
+
 let FOR = 		d3.hsl(240, 0.50, 0.30)
 let OPP = 		d3.hsl(000, 0.50, 0.30)
 
-let ABS = 		d3.hsl(240, 0.10, 0.30)
+let ABS = 		d3.hsl(120, 0.50, 0.30)
 
 let trumpA1DemFor = new Data(229, 		'D in favor',		{fill: DEM.formatHex().toString(), border: 'none'})
 let trumpA1DemOpp = new Data(2,			'D opposed',		{fill: DEM.formatHex().toString(), border: 'none'})
-let trumpA1DemAbs = new Data(1, 		'D abstained', 		{fill: ABS.formatHex().toString(), border: 'none'})
+let trumpA1DemAbs = new Data(2, 		'D abstained', 		{fill: ABS.formatHex().toString(), border: 'none'})
 
 let trumpA1RepFor = new Data(0,			'R in favor', 		{fill: REP.formatHex().toString(), border: 'none'})
 let trumpA1RepOpp = new Data(195,		'R opposed', 		{fill: REP.formatHex().toString(), border: 'none'})
@@ -149,7 +176,14 @@ let trumpA1IndAbs = new Data(0,			'I abstained', 		{fill: ABS.formatHex().toStri
 
 let trumpA1AllFor = new Data(230,		'Total in favor',	{fill: FOR.formatHex().toString(), border: 'none'})
 let trumpA1AllOpp = new Data(197, 		'Total opposed',	{fill: OPP.formatHex().toString(), border: 'none'})
-let trumpA1AllAbs = new Data(5, 		'Total abstained',	{fill: ABS.formatHex().toString(), border: 'none'})
+let trumpA1AllAbs = new Data(2, 		'Total abstained',	{fill: ABS.formatHex().toString(), border: 'none'})
+
+let TEST0 =			new Data(99, 		'Test 0',			{fill: TS0.formatHex().toString(), border: 'none'})
+let TEST1 =			new Data(40, 		'Test 1',			{fill: TS1.formatHex().toString(), border: 'none'})
+let TEST2 =			new Data(90, 		'Test 2',			{fill: TS2.formatHex().toString(), border: 'none'})
+
+
+trumpA1DemFor.addSubs([TEST0, TEST1, TEST2])
 
 trumpA1AllFor.addSubs([trumpA1DemFor, trumpA1RepFor, trumpA1IndFor])
 trumpA1AllOpp.addSubs([trumpA1DemOpp, trumpA1RepOpp, trumpA1IndOpp])
